e67770ea18104bff9adfa92da60eb8efe714343f
Haxorsprogramming <haxors@uinsu.ac.id>
2021-12-18 16:38:41 +0700

    'update'

---
+++ resources/views/home/homePage.blade.php
@@ -0,0 +1,459 @@
+<!DOCTYPE html>
+
+<html lang="en" class="minimal-style is-menu-fixed is-always-fixed is-selection-shareable blog-animated header-light header-small" data-effect="slideUp">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Impose - Responsive HTML5 Template">
+    <meta name="keywords" content="personal, blog, html5">
+    <meta name="author" content="Pixelwars">
+    <title>JustHasnah</title>
+
+    <!-- FAV and TOUCH ICONS -->
+    <link rel="shortcut icon" href="{{ asset('ladun/') }}/images/ico/favicon.ico">
+    <link rel="apple-touch-icon" href="{{ asset('ladun/') }}/images/ico/apple-touch-icon.png" />
+
+    <!-- FONTS -->
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,700|Noto+Sans:400,400i,700,700i|Poppins:300,400,500,600,700" rel="stylesheet">
+
+    <!-- STYLES -->
+    <link rel="stylesheet" type="text/css" href="{{ asset('ladun/') }}/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('ladun/') }}/css/fonts/fontello/css/fontello.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('ladun/') }}/js/jquery.magnific-popup/magnific-popup.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('ladun/') }}/js/jquery.fluidbox/fluidbox.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('ladun/') }}/js/owl-carousel/owl.carousel.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('ladun/') }}/js/selection-sharer/selection-sharer.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('ladun/') }}/css/rotate-words.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('ladun/') }}/css/align.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('ladun/') }}/css/main.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('ladun/') }}/js/shortcodes/shortcodes.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('ladun/') }}/css/768.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('ladun/') }}/css/992.css">
+
+    <!-- INITIAL SCRIPTS -->
+    <script src="{{ asset('ladun/') }}/js/modernizr.min.js"></script>
+
+</head>
+
+<body class="  ">
+
+    <!-- page -->
+    <div id="page" class="hfeed site">
+
+        <!-- header -->
+        <header id="masthead" class="site-header" role="banner">
+
+            <!-- site-navigation -->
+            <nav id="primary-navigation" class="site-navigation primary-navigation" role="navigation">
+
+                <!-- layout-medium -->
+                <div class="layout-medium">
+
+                    <!-- site-title : image-logo -->
+                    <h1 class="site-title">
+                        <a href="index.html" rel="home">
+                            <img src="{{ asset('ladun/') }}/images/site/logo.png" alt="logo">
+                            <span class="screen-reader-text">Haley Dust</span>
+                        </a>
+                    </h1>
+                    
+                    <a class="menu-toggle"><span class="lines"></span></a>
+
+                    <!-- nav-menu -->
+                    <div class="nav-menu">
+                        <ul>
+                            <li><a href="index.html">HOME</a></li>
+                            <li><a href="cat-archives.html">PORTFOLIO</a></li>
+                            <li><a href="cat-archives.html">LIFE</a></li>
+                            <li><a href="about.html">ABOUT ME</a></li>
+                            <li><a href="contact.html">CONTACT</a></li>
+                        </ul>
+                    </div>
+                    <!-- nav-menu -->
+
+                    <a class="search-toggle toggle-link"></a>
+
+                    <!-- search-container -->
+                    <div class="search-container">
+
+                        <div class="search-box" role="search">
+                            <form method="get" class="search-form" action="#">
+                                <label>Search Here
+                                    <input type="search" id="search-field" placeholder="type and hit enter" name="s">
+                                </label>
+                                <input type="submit" class="search-submit" value="Search">
+                            </form>
+                        </div>
+
+                    </div>
+                    <!-- search-container -->
+
+                    <!-- social-container -->
+                    <div class="social-container">
+
+                        <a class="social-link facebook" href="#!"></a>
+                        <a class="social-link twitter" href="#!"></a>
+                        <a class="social-link vine" href="#!"></a>
+                        <a class="social-link dribbble" href="#!"></a>
+                        <a class="social-link instagram" href="#!"></a>
+
+                    </div>
+                    <!-- social-container -->
+
+                </div>
+                <!-- layout-medium -->
+
+            </nav>
+            <!-- site-navigation -->
+
+        </header>
+        <!-- header -->
+
+        <!-- site-main -->
+        <div id="main" class="site-main">
+            <div class="layout-medium">
+                <div id="primary" class="content-area">
+
+                    <!-- site-content -->
+                    <div id="content" class="site-content" role="main">
+                        <!-- .hentry -->
+                        <article class="hentry page">
+
+                            <!-- .entry-content -->
+                            <div class="entry-content intro" data-animation="rotate-1">
+
+
+                                <!-- .profile-image -->
+                                <div class="profile-image">
+                                    <img alt="profile" src="{{ asset('ladun/') }}/images/site/about.jpg" />
+                                </div>
+                                <!-- .profile-image -->
+
+                                <h2><em>Hi.</em> I am Hasnah Ardita</h2>
+                                <h3>I am a <strong>Front End Dev.</strong> <strong>UI/UX Designer.</strong> <strong>Data Scients.</strong></h3>
+
+
+                                <!-- .link-boxes -->
+                                <figure>
+                                    <a href="about.html"><img src="{{ asset('ladun/') }}/images/site/box-01.jpg" alt="About Me"></a>
+                                    <figcaption class="wp-caption-text">About Me</figcaption>
+                                </figure>
+
+                                <figure>
+                                    <a href="about.html"><img src="{{ asset('ladun/') }}/images/site/box-02.jpg" alt="About Me"></a>
+                                    <figcaption class="wp-caption-text">Life</figcaption>
+                                </figure>
+
+                                <figure>
+                                    <a href="about.html"><img src="{{ asset('ladun/') }}/images/site/box-03.jpg" alt="About Me"></a>
+                                    <figcaption class="wp-caption-text">Travel</figcaption>
+                                </figure>
+
+                                <figure>
+                                    <a href="https://www.instagram.com/pixelwarsdesign/"><img src="{{ asset('ladun/') }}/images/site/box-04.jpg" alt="About Me"></a>
+                                    <figcaption class="wp-caption-text">Follow On Instagram</figcaption>
+                                </figure>
+                                <!-- .link-boxes -->
+
+
+                            </div>
+                            <!-- .entry-content -->
+
+
+
+                        </article>
+                        <!-- .page -->
+
+
+
+
+                        <!-- .home-title -->
+                        <h3 class="widget-title home-title">MY PORTFOLIO</h3>
+
+
+                        <!-- BLOG SIMPLE -->
+                        <div class="blog-simple">
+
+                            <!-- .hentry -->
+                            <article class="hentry post has-post-thumbnail">
+
+                                <!-- .hentry-left -->
+                                <div class="hentry-left">
+                                    <div class="entry-date">
+                                        <span class="day">17</span>
+                                        <span class="month">May</span>
+                                        <span class="year">2015</span>
+                                    </div>
+                                    <div class="featured-image" style="background-image:url('{{ asset('ladun/') }}/images/blog/08.jpg')"></div>
+                                </div>
+                                <!-- .hentry-left -->
+
+                                <!-- .hentry-middle -->
+                                <div class="hentry-middle">
+
+                                    <!-- .entry-title -->
+                                    <h2 class="entry-title"><a href="#!">SIM Ganesha</a></h2>
+
+                                </div>
+                                <!-- .hentry-middle -->
+
+                                
+
+                            </article>
+                            <!-- .hentry -->
+
+                            <!-- .hentry -->
+                            <article class="hentry post has-post-thumbnail">
+
+                                <!-- .hentry-left -->
+                                <div class="hentry-left">
+                                    <div class="entry-date">
+                                        <span class="day">03</span>
+                                        <span class="month">Apr</span>
+                                        <span class="year">2015</span>
+                                    </div>
+                                    <div class="featured-image" style="background-image:url('{{ asset('ladun/') }}/images/blog/08.jpg')"></div>
+                                </div>
+                                <!-- .hentry-left -->
+
+                                <!-- .hentry-middle -->
+                                <div class="hentry-middle">
+
+                                    <!-- .entry-title -->
+                                    <h2 class="entry-title"><a href="blog-single.html">Notes On Life</a></h2>
+
+                                </div>
+                                <!-- .hentry-middle -->
+
+                                <a class="post-link" href="blog-single.html">Notes On Life</a>
+
+                            </article>
+                            <!-- .hentry -->
+
+                            <!-- .hentry -->
+                            <article class="hentry post has-post-thumbnail">
+
+                                <!-- .hentry-left -->
+                                <div class="hentry-left">
+                                    <div class="entry-date">
+                                        <span class="day">09</span>
+                                        <span class="month">Jan</span>
+                                        <span class="year">2015</span>
+                                    </div>
+                                    <div class="featured-image" style="background-image:url('{{ asset('ladun/') }}/images/blog/08.jpg')"></div>
+                                </div>
+                                <!-- .hentry-left -->
+
+                                <!-- .hentry-middle -->
+                                <div class="hentry-middle">
+
+                                    <!-- .entry-title -->
+                                    <h2 class="entry-title"><a href="blog-single.html">Minimalist Living Tips</a></h2>
+
+                                </div>
+                                <!-- .hentry-middle -->
+
+                                <a class="post-link" href="blog-single.html">Minimalist Living Tips</a>
+
+
+                            </article>
+                            <!-- .hentry -->
+
+                            <!-- .hentry -->
+                            <article class="hentry post has-post-thumbnail">
+
+                                <!-- .hentry-left -->
+                                <div class="hentry-left">
+                                    <div class="entry-date">
+                                        <span class="day">14</span>
+                                        <span class="month">Jun</span>
+                                        <span class="year">2015</span>
+                                    </div>
+                                    <div class="featured-image" style="background-image:url('{{ asset('ladun/') }}/images/blog/09.jpg')"></div>
+                                </div>
+                                <!-- .hentry-left -->
+
+                                <!-- .hentry-middle -->
+                                <div class="hentry-middle">
+
+                                    <!-- .entry-title -->
+                                    <h2 class="entry-title"><a href="blog-single.html">History of The Beloved Machine</a></h2>
+
+                                </div>
+                                <!-- .hentry-middle -->
+
+                                <a class="post-link" href="blog-single.html">History of The Beloved Machine</a>
+
+
+                            </article>
+                            <!-- .hentry -->
+
+                            <!-- .hentry -->
+                            <article class="hentry post has-post-thumbnail">
+
+                                <!-- .hentry-left -->
+                                <div class="hentry-left">
+                                    <div class="entry-date">
+                                        <span class="day">25</span>
+                                        <span class="month">Agu</span>
+                                        <span class="year">2015</span>
+                                    </div>
+                                    <div class="featured-image" style="background-image:url('{{ asset('ladun/') }}/images/blog/10.jpg')"></div>
+                                </div>
+                                <!-- .hentry-left -->
+
+                                <!-- .hentry-middle -->
+                                <div class="hentry-middle">
+
+                                    <!-- .entry-title -->
+                                    <h2 class="entry-title"><a href="blog-single.html">Mastering Photography</a></h2>
+
+                                </div>
+                                <!-- .hentry-middle -->
+
+                                <a class="post-link" href="blog-single.html">Mastering Photography</a>
+
+
+                            </article>
+                            <!-- .hentry -->
+
+                        </div>
+                        <!-- BLOG SIMPLE -->
+
+
+
+                        <!-- .home-launch -->
+                        <div class="home-launch">
+                            <a class="button" href="blog-irregular.html">See All Posts</a>
+                        </div>
+                        <!-- .home-launch -->
+
+
+                    </div>
+                    <!-- site-content -->
+
+                </div>
+                <!-- primary -->
+
+
+
+
+
+            </div>
+            <!-- layout -->
+
+
+        </div>
+        <!-- site-main -->
+
+
+
+        <!-- site-footer -->
+        <footer id="colophon" class="site-footer" role="contentinfo">
+
+            <!-- layout-medium -->
+            <div class="layout-medium">
+
+
+                <!-- site-title-wrap -->
+                <div class="site-title-wrap">
+
+
+                    <!-- site-title : text-logo -->
+                    <!--<h1 class="site-title">
+                        <a href="../index.html" rel="home">
+                            Jeff Winger
+                        </a>
+                    </h1>-->
+                    <!-- site-title -->
+
+                    <!-- site-title : image-logo -->
+                    <h1 class="site-title">
+                        <a href="index.html" rel="home">
+                            <img src="{{ asset('ladun/') }}/images/site/logo.png" alt="logo">
+                        </a>
+                    </h1>
+                    <!-- site-title -->
+
+                    <p class="site-description">just living the life as it goes by</p>
+
+                </div>
+                <!-- site-title-wrap -->
+
+
+                <!-- footer-social -->
+                <div class="footer-social">
+
+                    <div class="textwidget">
+                        <a class="social-link facebook" href="#"></a>
+                        <a class="social-link twitter" href="#"></a>
+                        <a class="social-link vine" href="#"></a>
+                        <a class="social-link dribbble" href="#"></a>
+                        <a class="social-link instagram" href="#"></a>
+                    </div>
+
+                </div>
+                <!-- footer-social -->
+
+
+
+
+
+
+            </div>
+            <!-- layout-medium -->
+
+
+            <!-- .site-info -->
+            <div class="site-info">
+
+                <!-- layout-medium -->
+                <div class="layout-medium">
+
+                    <div class="textwidget">crafted with <i class="pw-icon-heart"></i> <em>by</em> Pixelwars</div>
+
+                </div>
+                <!-- layout-medium -->
+
+            </div>
+            <!-- .site-info -->
+
+
+
+        </footer>
+        <!-- site-footer -->
+
+
+    </div>
+    <!-- page -->
+
+
+    <!-- SCRIPTS -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script>
+        if (!window.jQuery) {
+            document.write('<script src="js/jquery-3.1.1.min.js"><\/script>');
+        }
+    </script>
+    <script src="{{ asset('ladun/') }}/js/jquery-migrate-3.0.0.min.js"></script>
+    <script src="{{ asset('ladun/') }}/js/fastclick.js"></script>
+    <script src="{{ asset('ladun/') }}/js/jquery.fitvids.js"></script>
+    <script src="{{ asset('ladun/') }}/js/jquery.viewport.mini.js"></script>
+    <script src="{{ asset('ladun/') }}/js/jquery.waypoints.min.js"></script>
+    <script src="{{ asset('ladun/') }}/js/jquery-validation/jquery.validate.min.js"></script>
+    <script src="{{ asset('ladun/') }}/js/imagesloaded.pkgd.min.js"></script>
+    <script src="{{ asset('ladun/') }}/js/jquery.isotope.min.js"></script>
+    <script src="{{ asset('ladun/') }}/js/jquery.magnific-popup/jquery.magnific-popup.min.js"></script>
+    <script src="{{ asset('ladun/') }}/js/jquery.fluidbox/jquery.fluidbox.min.js"></script>
+    <script src="{{ asset('ladun/') }}/js/owl-carousel/owl.carousel.min.js"></script>
+    <script src="{{ asset('ladun/') }}/js/selection-sharer/selection-sharer.js"></script>
+    <script src="{{ asset('ladun/') }}/js/socialstream.jquery.js"></script>
+    <script src="{{ asset('ladun/') }}/js/jquery.collagePlus/jquery.collagePlus.min.js"></script>
+    <script src="{{ asset('ladun/') }}/js/main.js"></script>
+    <script src="{{ asset('ladun/') }}/js/shortcodes/shortcodes.js"></script>
+
+</body>
+
+</html>
